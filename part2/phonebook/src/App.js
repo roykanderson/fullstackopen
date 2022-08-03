ffa583e0f157f3ec09c8e70237b482d2f6cb0abf
Roy Anderson <royanderson.k@gmail.com>
2022-08-03 13:07:10 -1000

    finished part2 exercises

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,40 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import personService from './services/personService'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import People from './components/People'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [color, setColor] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(people => {
+        setPersons(people)
       })
   }, [])
+
+  const flashNotification = (message, color) => {
+    setMessage(message)
+    setColor(color)
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={message} color={color} />
 
       <Filter value={filter} setFilter={setFilter}/>
 
@@ -34,11 +47,12 @@ const App = () => {
         setNewNumber={setNewNumber}
         persons={persons}
         setPersons={setPersons}
+        flashNotification={flashNotification}
       />
 
       <h2>Numbers</h2>
 
-      <People persons={persons} filter={filter} />
+      <People persons={persons} filter={filter} setPersons={setPersons} flashNotification={flashNotification}/>
     </div>
   )
 }
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,41 +1,73 @@
+import personService from '../services/personService'
+
 const PersonForm = ({ 
   newName,
   setNewName,
   newNumber,
   setNewNumber,
   persons,
-  setPersons
+  setPersons,
+  flashNotification
 }) => {
   
-  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNameChange = event => setNewName(event.target.value)
   
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleNumberChange = event => setNewNumber(event.target.value)
 
-  const submitPerson = (event) => {
+  const submitPerson = event => {
 
     event.preventDefault()
 
     const newPerson = { 
       name: newName, 
       number: newNumber,
-      id: 1 + persons.reduce((result, item) => {
-        return item.id > result
+      id: 1 + persons.reduce((result, item) => (
+        item.id > result
           ? item.id
           : result
-      }, 1)
+      ), 1)
     }
 
     // alert if person is already saved, otherwise add to phonebook
-    0 < persons.filter((person) => person.name === newPerson.name || person.number === newPerson.number).length
-      ? alert(`${newPerson.name} (${newPerson.number}) is already added to the phonebook`)
-      : setPersons(persons.concat(newPerson))
+    personExists(newPerson.name)
+      ? confirmPersonUpdate({ ...newPerson, id: getId(newPerson.name) })
+      : personService
+        .create(newPerson)
+        .then(newPerson => setPersons(persons.concat(newPerson)))
+        .then(() => flashNotification(`Added ${newPerson.name}`, 'green'))
 
     clearForm()
+  }
+
+  const confirmPersonUpdate = newPerson => {
+    if (window.confirm(`${newPerson.name} is already added to phonebook. Replace the old number with a new one?`))
+    {
+      personService
+        .update(newPerson)
+        .then(updatedPerson => 
+          setPersons(persons.map(person => 
+            person.id !== updatedPerson.id ? person : updatedPerson
+          ))
+        )
+        .then(() => flashNotification(`Updated ${newPerson.name}`, 'green'))
+        .catch(error => {
+          flashNotification(`Information of ${newPerson.name} has already been removed from the server.`, 'red')
+          setPersons(persons.filter(person => person.name !== newPerson.name))
+        })
+    }
   }
 
   const clearForm = () => {
     setNewName('')
     setNewNumber('')
+  }
+  
+  const personExists = (name) => {
+    return persons.filter(person => person.name === name).length > 0
+  }
+
+  const getId = name => {
+    return persons.find(person => person.name === name).id
   }
 
   return (
